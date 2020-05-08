52dfc2a4c5012275c668e086f525a3933b9edad3
Ricardo Pires <Ricky@MacBook-Pro-de-ricardo.local>
2020-05-08 15:56:19 +0200

    a

---
+++ src/NoMatch.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const NoMatch = () => (
+    <div>
+        <h2>NoMatch</h2>
+
+
+
+
+    </div>
+
+
+
+
+
+)
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Nav, Navbar } from "react-bootstrap";
+import styled from 'styled-components'
+
+const Styles = styled.div`
+
+.navbar{
+
+    background-color: #222;
+}
+
+.navbar-brand, .navbar-nav .nav-kink{
+color: #bbb;
+
+&:hover{
+    color: white;
+}
+}
+`;
+
+
+
+export const NavigationBar = () => (
+
+    <Styles>
+
+        <Navbar expand="lg">
+
+            <Navbar.Brand href="/">Code Life</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+            </Navbar.Collapse>
+            <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link href="/contact">Contact</Nav.Link></Nav.Item>
+        </Navbar>
+
+    </Styles>
+
+)
